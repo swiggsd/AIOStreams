acaff9625cbe0351721d3582698cbe4e90d01786
Root <50879691+swiggsd@users.noreply.github.com>
2025-06-01 02:02:55 +0530

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -175,7 +175,7 @@ export class BaseWrapper {
     );
 
     let response = useProxy
-      ? uFetch(url, {
+      ? Fetch(url, {
           dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
           method: 'GET',
           headers: this.headers,
